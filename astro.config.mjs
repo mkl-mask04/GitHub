01cbc7ebb08d7d35b590f3fca35520844f95061a
mkl-mask04 <mkl20398@hawkmail.hacc.edu>
2025-03-05 21:18:42 -0500

    KMS

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/assignments/assignment-5',
+  base: '/exercise-6/',
   build: {
-    site: 'masonklopez.com/exercise-6',
+    site: 'masonklopez.com/exercise-6/',
   }
 });
