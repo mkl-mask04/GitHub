c4e2d3ad2ddaddfafaf5182ff41b159b7df8031b
mkl-mask04 <mkl20398@hawkmail.hacc.edu>
2025-03-05 20:07:58 -0500

    maybe finished tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  base: '/assignments/assignment-5',
+  build: {
+    site: 'masonklopez.com/exercise-6',
+  }
+});
